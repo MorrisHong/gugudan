72934ef2ad3c59e2ab9df624596f84dd058ce2da
Administrator <Administrator@MSDN-SPECIAL>
2019-01-20 09:40:29 +0900

    Hi

---
+++ src/GugudanNogada.java
@@ -0,0 +1,54 @@
+
+public class GugudanNogada {
+	public static void main(String[] args) {
+		//2단
+		System.out.println("2단");
+		System.out.println(2 * 1);
+		System.out.printf("%d\n", 2*2);
+		System.out.print(2*3+"\n");
+		System.out.print(2*4+"\n");
+		System.out.print(2*5+"\n");
+		System.out.print(2*6+"\n");
+		System.out.print(2*7+"\n");
+		System.out.print(2*8+"\n");
+		System.out.print(2*9+"\n\n");
+		
+
+		//3단
+		System.out.println("3단");
+		System.out.println(3*1);
+		System.out.println(3*2); 
+		System.out.println(3*3);
+		System.out.println(3*4);
+		System.out.println(3*5);
+		System.out.println(3*6);
+		System.out.println(3*7);
+		System.out.println(3*8);
+		System.out.println(3*9);
+		System.out.println();
+		
+		//4단
+		System.out.println("4단");
+		int result = 4 * 1;
+		System.out.println(result);
+		result = 4 * 2;
+		System.out.println(result);
+		result = 4 * 3;
+		System.out.println(result);
+		result = 4 * 4;
+		System.out.println(result);
+		result = 4 * 5;
+		System.out.println(result);
+		result = 4 * 6;
+		System.out.println(result);
+		result = 4 * 7;
+		System.out.println(result);
+		result = 4 * 8;
+		System.out.println(result);
+		result = 4 * 9;
+		System.out.println(result);
+		System.out.println();
+		
+		
+	}
+}
